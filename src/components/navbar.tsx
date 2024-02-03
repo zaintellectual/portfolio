d269787d1a2966d5b6cbb9a17a278edba6e4cd14
zaintellectual <zaintellectual@gmail.com>
2024-02-03 07:15:22 -0800

    update

---
+++ src/components/navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <header className="text-gray-600 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    {/* <Image src={require("../../public/assests/Pic/zain.jpg")} alt="Intellectual" width={100} height={100} /> */}
+      <span className="ml-3 text-xl">Zain Abbas</span>
+    </a>
+    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+      <a className="text-bold mr-5 hover:text-red-500">Home</a>
+      <a className="text-bold mr-5 hover:text-red-500">About</a>
+      <a className="text-bold mr-5 hover:text-red-500">Project</a>
+      <a className="text-bold mr-5 hover:text-red-500">Contact</a>
+    </nav>
+    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Download CV
+      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+</header>
+    </div>
+  )
+}
+
+export default Navbar
